29b8889d4236929b8f1a8e19448618ca48eff18b
phenix1229 <paul.garay@codeimmersives.com>
2019-12-02 13:55:03 -0500

    done

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
-const makeDino = function() {
+const makeDino = function(species, period, carnivore, extinct = false) {
+  return {
+    species,
+    period,
+    carnivore,
+    extinct,
+  }
 }
 
-const makeSingular = function() {
+const makeSingular = function(dino) {
+  const newDino = {
+    species: dino.species.slice(0, dino.species.length -2),
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: dino.extinct,
+  }
+  return dino.species.endsWith('us') ? newDino : dino;
 }
 
-const truncateSpecies = function() {
+const truncateSpecies = function(dino) {
+  const newDino = {
+    species: `${dino.species.slice(0, 7)}...`,
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: dino.extinct,
+  }
+  return dino.species.length <= 10 ? dino : newDino;
 }
 
-const makeExtinct = function() {
-
+const makeExtinct = function(dino) {
+  const newDino = {
+    species: dino.species,
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: true,
+  }
+  return newDino;
 }
 
 module.exports = {
